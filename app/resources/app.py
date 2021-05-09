076f078db58aa9a2df7f40b6101de0e6cbff1143
Paulo Victor Moura <paulovsm@gmail.com>
2021-05-09 11:33:07 -0300

    Adding web app implementation

--- app/resources/app.py
+++ app/resources/app.py
@@ -96,14 +96,14 @@ class App:
         if (normalize):
             transform_pipeline = transforms.Compose([
                 transforms.Resize((224, 224)),
-                transforms.CenterCrop(224)
+                transforms.CenterCrop(224),
                 transforms.ToTensor(),
                 transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225])
             ])
         else:
             transform_pipeline = transforms.Compose([
                 transforms.Resize((224, 224)),
-                transforms.CenterCrop(224)
+                transforms.CenterCrop(224),
                 transforms.ToTensor()
             ])
 
